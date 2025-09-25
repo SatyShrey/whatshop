d9b3152affa350a8a10f5f0e0084dbf91aaaf251
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-26 02:25:29 +0530

    __

--- netlify/functions/app.cjs
+++ netlify/functions/app.cjs
@@ -40,7 +40,7 @@ app.post('/api/login', async (req, res) => {
     const fakeotp = Math.floor(1000 + Math.random() * 9000).toString();
     await User.updateOne({email},{otp:fakeotp})
     const token = jwt.sign(user.toObject(), secret, { expiresIn: "30d" })
-    res.cookie("hiUser", token, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 })
+    res.cookie("hiUser", token, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000,sameSite:"none",secure:true })
     res.send({ email: user.email, name: user.name });
   } catch (error) { res.status(502).send(error.message);console.log(error.message) }
 })
