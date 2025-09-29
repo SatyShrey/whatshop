6eeb2e5dbf6dfd143a899a5d535b6db28fa5dd89
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-30 01:01:01 +0530

    __

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
   const {theme}=useValues();
 
   return (
-    <div className="shadow-[0_0_2px] h-dvh flex flex-col overflow-hidden"
+    <div id="app" className="shadow-[0_0_2px] h-dvh flex flex-col overflow-hidden"
      data-theme={theme?"night":"light"}>
       {!shouldHideNavbar && <Header/>}
          <Routes>
